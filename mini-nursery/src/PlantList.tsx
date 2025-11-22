38efe547c7a05f255e4aea7faa5c150667dcbdb9
Buddhika Senevirathne <senevir-ps21060@stu.kln.ac.lk>
2025-11-22 22:13:00 +0530

    second commit

---
+++ mini-nursery/src/PlantList.tsx
@@ -0,0 +1,28 @@
+
+import { Plant } from "./types";
+
+type Props = {
+  plants: Plant[];
+  selectedId: number | null;
+  onSelect: (id: number) => void;
+};
+
+export default function PlantList({ plants, selectedId, onSelect }: Props) {
+  return (
+    <ul className="list-group">
+      {plants.map((p) => (
+        <li
+          key={p.id}
+          className={`list-group-item ${selectedId === p.id ? "active" : ""}`}
+          style={{ cursor: "pointer" }}
+          onClick={() => onSelect(p.id)}
+        >
+          <strong>{p.name}</strong> <br />
+          <small>{p.scientific}</small> <br />
+          <span className="badge bg-primary me-2">{p.difficulty}</span>
+          <span className="badge bg-warning">{p.rarity}</span>
+        </li>
+      ))}
+    </ul>
+  );
+}
--- mini-nursery/src/main.tsx
+++ mini-nursery/src/main.tsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { Plant } from "./types";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const plants: Plant[] = [
+  {
+    id: 1,
+    name: "Snake Plant",
+    scientific: "Sansevieria trifasciata",
+    difficulty: "Easy",
+    light: "Low",
+    water: "Low",
+    rarity: "Common",
+    description: "Very easy to care for. Good for beginners."
+  },
+  {
+    id: 2,
+    name: "Monstera",
+    scientific: "Monstera Deliciosa",
+    difficulty: "Medium",
+    light: "Bright",
+    water: "Moderate",
+    rarity: "Uncommon",
+    description: "Large, beautiful leaves with natural holes."
+  },
+  {
+    id: 3,
+    name: "Peace Lily",
+    scientific: "Spathiphyllum",
+    difficulty: "Easy",
+    light: "Medium",
+    water: "Frequent",
+    rarity: "Common",
+    description: "Popular indoor plant with white flowers."
+  }
+];
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App plants={plants} />
+  </React.StrictMode>,
 )
